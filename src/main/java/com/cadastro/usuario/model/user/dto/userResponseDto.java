ef66b265d62129d109aa951895d486a19d0420c0
dianinecosta-wq <dianine.costa@zipdin.com.br>
2025-03-08 20:12:41 -0300

    feat: add class userResponseDto

---
+++ src/main/java/com/cadastro/usuario/model/user/dto/userResponseDto.java
@@ -0,0 +1,14 @@
+package com.cadastro.usuario.model.user.dto;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@EqualsAndHashCode
+public class userResponseDto {
+    private String name;
+    private String cpf;
+    private String email;
+}
